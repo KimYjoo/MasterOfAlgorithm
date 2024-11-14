7e4197329358fc08acb44da80ac822cbcb2f3453
odeg2ya <pas4976@gmail.com>
2024-11-14 14:03:53 +0900

    B1929/코드/첫시도 코드(실패, 시간초과)

--- silver/B1929/B1929.py
+++ silver/B1929/B1929.py
@@ -1,10 +1,28 @@
-import sys
+def slv(minimum, maximum):
+    prime_numbers = []
+    rslt_prime_numbers = []
+    for i in range(2, minimum) :
+        check_prime_number(i, prime_numbers)
+    for i in range(minimum, maximum + 1):
+        get_rslt_prime_number(i, prime_numbers, rslt_prime_numbers)
+    result = '\n'.join(rslt_prime_numbers)
+    print(result)
 
-N, M = sys.stdin.readline().strip().split(' ')
+def check_prime_number(number, prime_numbers) :
+    for i in prime_numbers :
+        if number % i == 0 :
+            return
+    prime_numbers.append(number)
+    return
 
-print(f'{N} ,{M}')
+def get_rslt_prime_number(number, prime_numbers, rslt_prime_numbers) :
+    for i in prime_numbers :
+        if number % i == 0 :
+            return
+    prime_numbers.append(number)
+    rslt_prime_numbers.append(str(number))
+    return
 
-
-pre = []
-
-for i in
+if __name__ == "__main__":
+    M, N = map(int, input().split())
+    slv(M,N)

9d72514f32cecafbbc9209ca670d34d4543a92ac
KimYjoo <pas4976@gmail.com>
2025-04-11 15:56:37 +0900

    25/04/11

--- tes.py
+++ tes.py
@@ -1,7 +1,12 @@
 import sys
 
-T = int(sys.stdin.readline().strip())
+a,b,c = map(int, sys.stdin.readline().split())
 
-for i in range(1, T+1):
-    a, b = map(int, sys.stdin.readline().split())
-    print(f'Case #{i}: {a+b}')
+if a == b == c:
+    print(10000 + a * 1000)
+elif a == b or a == c:
+    print(1000 + a * 100)
+elif b == c:
+    print(1000 + b * 100)
+else:
+    print(max([a,b,c]) * 100)
